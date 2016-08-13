db020dc6685ed810a94fc6c02c5fa7f69dbaff0d
Slawomir Wdowka <swdowka@gmail.com>
2016-08-13 13:49:21 +0100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Badge.create([
+  {"name"=>"Environment", "image_url"=>"https://www.girlguiding.org.uk/brownies/badges/images/environment.gif"},
+  {"name"=>"First aid ", "image_url"=>"https://www.girlguiding.org.uk/brownies/badges/images/firstaid.gif"},
+  {"name"=>"Art", "image_url"=>"https://www.girlguiding.org.uk/brownies/badges/images/artist.gif"},
+  {"name"=>"Wildlife ", "image_url"=>"https://www.girlguiding.org.uk/brownies/badges/images/wildlifeExplorer.gif"},
+  {"name"=>"Recycling", "image_url"=>"https://www.girlguiding.org.uk/brownies/badges/images/worldIssues.gif"}
+])
